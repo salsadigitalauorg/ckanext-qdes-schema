08ec3b1021c9e84792f81ab7d6b531c29795f3c8
awang setyawan <awang@salsadigital.com.au>
2020-08-25 14:31:52 +0700

    [DDCI-35] - added identifiers field

--- ckanext/qdes_schema/fanstatic/repeating-fields.js
+++ ckanext/qdes_schema/fanstatic/repeating-fields.js
@@ -8,7 +8,11 @@ jQuery(document).ready(function () {
         },
         hide: function (deleteElement) {
             if(confirm('Are you sure you want to remove this item?')) {
+                jQuery(this).find('.form-control').val('');
                 jQuery(this).hide(deleteElement);
+
+                // Trigger blur event to regenerate the field value.
+                jQuery('.repeating-field .form-control').blur();
             }
         },
         ready: function (setIndexes) {
@@ -20,14 +24,23 @@ jQuery(document).ready(function () {
     function collate_inputs(repeater_id, target_field_id, field_type) {
         var collated_values = [];
 
-        jQuery('#' + repeater_id + ' ' + field_type).each(function() {
-            collated_values.push(jQuery(this).val());
+        jQuery('#' + repeater_id + ' ' + field_type + '.form-control').each(function() {
+            var value = jQuery(this).val();
+
+            if (value.trim().length > 0) {
+                collated_values.push(jQuery(this).val());
+            }
         });
 
-        jQuery('#' + target_field_id).val(JSON.stringify(collated_values));
+        if (collated_values.length > 0) {
+            jQuery('#' + target_field_id).val(JSON.stringify(collated_values));
+        }
+        else {
+            jQuery('#' + target_field_id).val('');
+        }
     }
 
-    jQuery(document).on('blur', ".repeating-field textarea", function() {
+    jQuery(document).on('blur', ".repeating-field .form-control", function() {
         var repeater_id = jQuery(this).attr('id').replace('field-item-', '') + '-repeater';
         var target_field_id = jQuery(this).attr('id').replace('-item-', '-');
         var field_type = jQuery('#' + repeater_id).data('field-type');
