59db604f182e1dff95d5695b7bd31436334b61c5
awang setyawan <awang@salsadigital.com.au>
2020-08-26 11:46:29 +0700

    [DDCI-115] - added temporal dates validation

--- ckanext/qdes_schema/validators.py
+++ ckanext/qdes_schema/validators.py
@@ -1,7 +1,8 @@
 import ckan.plugins.toolkit as toolkit
 
+def qdes_temporal_start_end_date(key, flattened_data, errors, context):
+    temporal_start_value = flattened_data[('temporal_start',)]
+    temporal_end_value = flattened_data[('temporal_end',)]
 
-def qdes_temporal_start_end_date(field, schema):
-    if len(field) > 300:
-        raise toolkit.Invalid("Max chars should be 3 characters")
-    return field
+    if ((len(temporal_start_value) > 0) != (len(temporal_end_value) > 0)) and (len(flattened_data.get(key)) == 0):
+        raise toolkit.Invalid("This field should not be empty")
