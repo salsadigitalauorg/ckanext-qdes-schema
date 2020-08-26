bc456362d35fe427deaa46d2c5332df7010053ac
awang setyawan <awang@salsadigital.com.au>
2020-08-26 12:00:36 +0700

    [DDCI-115] - added validation for start < end date

--- ckanext/qdes_schema/validators.py
+++ ckanext/qdes_schema/validators.py
@@ -1,4 +1,5 @@
 import ckan.plugins.toolkit as toolkit
+from datetime import datetime as dt
 
 def qdes_temporal_start_end_date(key, flattened_data, errors, context):
     temporal_start_value = flattened_data[('temporal_start',)]
@@ -6,3 +7,10 @@ def qdes_temporal_start_end_date(key, flattened_data, errors, context):
 
     if ((len(temporal_start_value) > 0) != (len(temporal_end_value) > 0)) and (len(flattened_data.get(key)) == 0):
         raise toolkit.Invalid("This field should not be empty")
+
+
+    if dt.strptime(temporal_start_value, '%Y-%m-%d') > dt.strptime(temporal_end_value, '%Y-%m-%d'):
+        if key == ('temporal_start',):
+            raise toolkit.Invalid("Start date must be less then end date.")
+        elif key == ('temporal_end',):
+            raise toolkit.Invalid("End date must be higher then start date.")
