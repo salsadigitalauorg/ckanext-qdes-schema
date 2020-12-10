fd2da29cc035e35289535ca7f1390ea183767dab
awang setyawan <awang@salsadigital.com.au>
2020-12-10 16:09:13 +0700

    [DDCI-204] - updated data service to id and added delete mechanism

--- ckanext/qdes_schema/plugin_resources.py
+++ ckanext/qdes_schema/plugin_resources.py
@@ -1,7 +1,9 @@
 import ckan.plugins as plugins
+import logging
 
 from ckan.plugins.toolkit import get_action
 
+log = logging.getLogger(__name__)
 
 class QDESSchemaResourcesPlugin(plugins.SingletonPlugin):
     """
@@ -12,7 +14,16 @@ class QDESSchemaResourcesPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IResourceController, inherit=True)
 
     def after_create(self, context, resource):
-        get_action('update_dataservice_datasets_available')(context, resource)
+        get_action('update_dataservice_datasets_available')(context, {'resource': resource})
 
     def after_update(self, context, resource):
-        get_action('update_dataservice_datasets_available')(context, resource)
+        get_action('update_dataservice_datasets_available')(context, {'resource': resource})
+
+    def before_delete(self, context, resource, resources):
+        for res in resources:
+            if res.get('id') == resource.get('id'):
+                get_action('update_dataservice_datasets_available')(context, {
+                    'resource': res,
+                    'resource_deleted': True,
+                    'resources': resources,
+                })
