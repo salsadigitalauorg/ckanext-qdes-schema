dff1cdaa53f6756eed0c78c05539757913403ddd
Mark Calvert <markcalvert84@gmail.com>
2021-02-08 16:42:37 -0500

    Harvest helper udates: - Updated 'get_vocabulary_service_term' to use new action 'get_vocabulary_service_term' to find exact term instead of using the search which does a contains search - Refactored 'convert_size_to_bytes' into helper
    Updated file 'DES_Datasets_Open_data_v1.csv' to the latest version
    
    Updated qld harvester to use csv file to set default values
    Updated code to handle existing packages and business rules
    
    Fixed issues with qspatial harvester with parent/child packages relataionships

--- ckanext/qdes_schema/logic/helpers/harvest_helpers.py
+++ ckanext/qdes_schema/logic/helpers/harvest_helpers.py
@@ -7,17 +7,17 @@ def get_mapped_update_frequency(update_frequency):
     # https://www.data.qld.gov.au/organization/a3cdcdcb-201c-4360-9fa6-98e361c89279?update_frequency=not-updated.
     # @todo: fix this.
     frequency_map = {
-        'near-real-time': 'http://registry.it.csiro.au/def/isotc211/MD_MaintenanceFrequencyCode/continual',
+        'near-real-time': 'http://publications.europa.eu/resource/authority/continual',
         'hourly': '',
-        'daily': 'http://registry.it.csiro.au/def/isotc211/MD_MaintenanceFrequencyCode/daily',
-        'weekly': 'http://registry.it.csiro.au/def/isotc211/MD_MaintenanceFrequencyCode/weekly',
-        'fortnightly': 'http://registry.it.csiro.au/def/isotc211/MD_MaintenanceFrequencyCode/fortnightly',
-        'monthly': 'http://registry.it.csiro.au/def/isotc211/MD_MaintenanceFrequencyCode/monthly',
-        'quarterly': 'http://registry.it.csiro.au/def/isotc211/MD_MaintenanceFrequencyCode/quarterly',
-        'half-yearly': 'http://registry.it.csiro.au/def/isotc211/MD_MaintenanceFrequencyCode/biannually',
-        'annually': 'http://registry.it.csiro.au/def/isotc211/MD_MaintenanceFrequencyCode/annually',
-        'non-regular': 'http://registry.it.csiro.au/def/isotc211/MD_MaintenanceFrequencyCode/irregular',
-        'not-updated': 'http://registry.it.csiro.au/def/isotc211/MD_MaintenanceFrequencyCode/notPlanned'
+        'daily': 'http://publications.europa.eu/resource/authority/daily',
+        'weekly': 'http://publications.europa.eu/resource/authority/weekly',
+        'fortnightly': 'http://publications.europa.eu/resource/authority/fortnightly',
+        'monthly': 'http://publications.europa.eu/resource/authority/monthly',
+        'quarterly': 'http://publications.europa.eu/resource/authority/quarterly',
+        'half-yearly': 'http://publications.europa.eu/resource/authority/biannually',
+        'annually': 'http://publications.europa.eu/resource/authority/annually',
+        'non-regular': 'http://publications.europa.eu/resource/authority/irregular',
+        'not-updated': 'http://publications.europa.eu/resource/authority/notPlanned'
     }
     return frequency_map.get(update_frequency, '')
 
@@ -45,22 +45,21 @@ def get_secure_vocabulary_record(destination, query, vocabulary_name, debug=Fals
         print(str(e))
 
 
-def get_vocabulary_service_term(destination, query, vocabulary_service_name, debug=False):
+def get_vocabulary_service_term(destination, term_label, vocabulary_service_name, debug=False):
     try:
         data_dict = {
-            'q': query,
-            'term_name': vocabulary_service_name,
-            'limit': 1,
+            'vocabulary_service_name': vocabulary_service_name,
+            'term_label': term_label
         }
-        result = destination.action.vocabulary_service_term_search(**data_dict)
+        result = destination.action.get_vocabulary_service_term(**data_dict)
 
         # Only return the first result, as presumably the data in the CSV file will be a unique name
-        if result and len(result) >= 1:
-            return result[0].get('value', None)
+        if result and isinstance(result, dict):
+            return result.get('uri', None)
 
         if debug:
             print('>>>')
-            print('No term found in vocabulary_service: {0} for query: {1}'.format(vocabulary_service_name, query))
+            print('No term found in vocabulary_service: {0} for term_label_or_uri: {1}'.format(vocabulary_service_name, term_label))
             print('>>>')
     except Exception as e:
         print(str(e))
@@ -83,3 +82,68 @@ def fix_url(url):
         ))
 
     return url
+
+
+def convert_size_to_bytes(size_str):
+    """
+    Convert human file-sizes to bytes.
+    """
+    multipliers = {
+        'kib': 1024,
+        'mib': 1024 ** 2,
+        'gib': 1024 ** 3,
+    }
+
+    # Some Nones observed during migration
+    if not size_str:
+        return 0
+
+    # Some Int values observed for size
+    if isinstance(size_str, int):
+        return size_str
+
+    try:
+        for suffix in multipliers:
+            size_str = size_str.replace(',', '')
+            size_str = size_str.lower().strip().strip('s')
+            if size_str.lower().endswith(suffix):
+                return int(float(size_str[0:-len(suffix)]) * multipliers[suffix])
+        else:
+            if size_str.endswith('b'):
+                size_str = size_str[0:-1]
+            elif size_str.endswith('byte'):
+                size_str = size_str[0:-4]
+        return int(size_str)
+    except Exception as e:
+        print('Exception raised in convert_size_to_bytes')
+        print(f'>>> Input: {size_str}')
+        print('Exception: {}'.format(str(e)))
+        raise e
+
+def get_mapped_format(format):
+    format_map = {
+        'xml': 'http://publications.europa.eu/resource/authority/file-type/XML',
+        'csv': 'http://publications.europa.eu/resource/authority/file-type/CSV',
+        'csv validation schema': 'http://publications.europa.eu/resource/authority/file-type/CSV',
+        'txt': 'http://publications.europa.eu/resource/authority/file-type/TXT',
+        'json': 'http://publications.europa.eu/resource/authority/file-type/JSON',
+        'pdf': 'http://publications.europa.eu/resource/authority/file-type/PDF',
+        'xls': 'http://publications.europa.eu/resource/authority/file-type/XLSX', #XLS
+        'xlsx': 'http://publications.europa.eu/resource/authority/file-type/XLSX',
+        'docx': 'http://publications.europa.eu/resource/authority/file-type/DOCX',
+        'kmz': 'http://publications.europa.eu/resource/authority/file-type/KMZ',
+        'kml': 'http://publications.europa.eu/resource/authority/file-type/KML',
+        'shp': 'http://publications.europa.eu/resource/authority/file-type/SHP',
+        'geotiff': 'http://publications.europa.eu/resource/authority/file-type/TIFF', #geotiff
+        'tif': 'http://publications.europa.eu/resource/authority/file-type/TIFF',
+        'rest': 'http://publications.europa.eu/resource/authority/file-type/REST',
+        'geojson': 'http://publications.europa.eu/resource/authority/file-type/GEOJSON',
+        'html': 'http://publications.europa.eu/resource/authority/file-type/HTML_SIMPL',
+        'spatial data format': 'http://publications.europa.eu/resource/authority/file-type/GDB',
+        'shp, tab, fgdb, kmz, gpkg': 'http://publications.europa.eu/resource/authority/file-type/GDB'
+    }
+    # ZIP needs to be handled manually
+    return format_map.get(format.lower(), '')
+
+
+	
