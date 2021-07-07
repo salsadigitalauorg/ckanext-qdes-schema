a3e8efa0236a01303dd1c9bec8048b4018824974
Mark Calvert <markcalvert84@gmail.com>
2021-07-07 11:05:56 -0600

    [DDCI-989] - Override CKAN core dataset view class CreateView - Add logic to save dataset in draft state when the 'Finish: Save record' is clicked and redirect to dataset read page

--- ckanext/qdes_schema/fanstatic/dataset_no_resource.js
+++ ckanext/qdes_schema/fanstatic/dataset_no_resource.js
@@ -1,7 +1,11 @@
 jQuery(document).ready(function () {
-    jQuery('button[name="save_record"]').on('click', function (e) {
+    jQuery('button[name="save_record"]').on("click", function (e) {
         e.preventDefault();
-        jQuery('input[name="_ckan_phase"]').remove();
-        jQuery(this).closest('form').submit();
+        if (jQuery('input[name="pkg_name"]').val().length > 0) {
+            jQuery('input[name="_ckan_phase"]').val("save_record");
+        } else {
+            jQuery('input[name="_ckan_phase"]').remove();
+        }
+        jQuery(this).closest("form").submit();
     });
 });
