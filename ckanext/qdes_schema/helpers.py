f51b81e468d7a4f5a4a16dae078641ac6f1e48ba
awang setyawan <awang@salsadigital.com.au>
2021-08-12 21:36:03 +0700

    [SUPDESQ-19] - removed backward compatibility for publishing

--- ckanext/qdes_schema/helpers.py
+++ ckanext/qdes_schema/helpers.py
@@ -520,12 +520,9 @@ def map_update_schedule(uri, schema):
 def map_license(uri, schema):
     license_map = {
         constants.PUBLISH_EXTERNAL_IDENTIFIER_DATA_QLD_SCHEMA: {
-            'https://linked.data.gov.au/def/licence-document/cc-by-4.0': 'cc-by-4',
-            'https://linked.data.gov.au/def/licence-document/cc-by-nd-4.0': 'cc-by-nd-4',
-            'https://linked.data.gov.au/def/licence-document/cc-by-sa-4.0': 'cc-by-sa-4',
             'https://linked.data.gov.au/def/qld-data-licenses/cc-by-4.0': 'cc-by-4',
             'https://linked.data.gov.au/def/qld-data-licenses/cc-by-nd-4.0': 'cc-by-nd-4',
-            'https://linked.data.gov.au/def/qld-data-licenses/cc-by-sa-4.0': 'cc-by-sa-4',
+            'https://linked.data.gov.au/def/qld-data-licenses/cc-by-sa-4.0': 'cc-by-sa-4'
         },
         # @todo, in case needed, need to map this against external schema in future.
         constants.PUBLISH_EXTERNAL_IDENTIFIER_QSPATIAL_SCHEMA: {},
