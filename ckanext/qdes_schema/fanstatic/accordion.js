6021f1f41332a72ec9b377777309d95c4f894a49
Konstantin Sivakov <konstantin.sivakov@gmail.com>
2021-11-16 23:53:04 +0100

    Check for open/close toggle

--- ckanext/qdes_schema/fanstatic/accordion.js
+++ ckanext/qdes_schema/fanstatic/accordion.js
@@ -3,7 +3,8 @@ jQuery(document).ready(function () {
   document.addEventListener('keyup', function (e) {
     console.log(e.key);
     if (e.key === 'Enter' || e.key === ' ' || e.key === 'Spacebar') {
-      document.activeElement.parentNode.querySelector("input[type]").checked = "true";
+      var checked = document.activeElement.parentNode.querySelector("input[type]").checked;
+      document.activeElement.parentNode.querySelector("input[type]").checked = !checked;
     };
 
     e.preventDefault();
