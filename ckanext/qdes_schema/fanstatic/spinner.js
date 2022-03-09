41e91d8cddc48899514bceddb9224ba9bf3a61ea
Awang <awang@salsadigital.com.au>
2022-03-09 08:06:33 +0700

    [SUPDESQ-47] - fix spinner issue (#230)

--- ckanext/qdes_schema/fanstatic/spinner.js
+++ ckanext/qdes_schema/fanstatic/spinner.js
@@ -1,12 +1,10 @@
-jQuery(document).ready(function () {
-    var $loaderEl = $('.loader');
-    $(window).on('load', function () {
-        removeLoader();
+function removeLoader() {
+    var $loaderEl = jQuery('.loader');
+    $loaderEl.fadeOut(500, function () {
+        // fadeOut complete. Remove the loading div
+        $loaderEl.remove(); //makes page more lightweight
     });
-    function removeLoader() {
-        $loaderEl.fadeOut(500, function () {
-            // fadeOut complete. Remove the loading div
-            $loaderEl.remove(); //makes page more lightweight
-        });
-    }
+}
+jQuery(window).on('load', function () {
+    removeLoader();
 });
